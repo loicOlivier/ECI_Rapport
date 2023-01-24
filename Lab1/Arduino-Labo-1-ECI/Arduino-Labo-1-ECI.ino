bdd5b2f096ce9d2295d0669208dd517b98d8c24a
loicOlivier <93949230+loicOlivier@users.noreply.github.com>
2023-01-24 18:44:26 -0500

    Macros dans le arduino + protocole surligné pdf
    GG

--- Lab1/Arduino-Labo-1-ECI/Arduino-Labo-1-ECI.ino
+++ Lab1/Arduino-Labo-1-ECI/Arduino-Labo-1-ECI.ino
@@ -1,3 +1,9 @@
+// Modes d'utilisations
+// Définir le mode désiré uniquement et commenter l'autre
+
+#define SCOPE
+//#define SQUARE_WAVE
+
 void setup() {
   // put your setup code here, to run once:
   //2)
@@ -10,15 +16,34 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   //2)
+  #ifdef SCOPE
   int data = analogRead(A0); //Lecture analogique et conversion numérique de la tension lue sur A0
   Serial.println(data); //Affichage de data dans le moniteur de communication série
-  delay(100); //Délai ajouté pour échantillonner à une fréquence de 10 Hz
-
+  delay(100); //Délai de 100ms ajouté pour échantillonner à une fréquence de 10 Hz
+  #endif
+  
   //3)
-  //digitalWrite(3, HIGH); //Mise à un haut logique (5V) de la pin 3
-  //delay(0.5); //Délai ajouté pour imposer une fréquence de 1kHz à la boucle
-  //digitalWrite(3, LOW); //Mise à un bas logique (0V) de la pin 3
-  //delay(0.5); //Délai ajouté pour imposer une fréquence de 1kHz à la boucle
+  #ifdef SQUARE_WAVE
 
+  // Acquisition (scope)
+  int data = analogRead(A0); //Lecture analogique et conversion numérique de la tension lue sur A0
+  Serial.println(data); //Affichage de data dans le moniteur de communication série
+
+  // Génération d'une onde carré à 1kz
+  /* Remarque: La fréquence réelle mesurée sera inférieure à 1kz en raison 
+   * des autres fonctions (digitalWrite, Serial.print, etc.) qui prennet
+   * une courte durée à être effectué.
+  */ 
+  digitalWrite(3, HIGH); //Mise à un haut logique (5V) de la pin 3
+  delay(0.5); //Délai ajouté pour imposer une fréquence de 1kHz à la boucle
+  digitalWrite(3, LOW); //Mise à un bas logique (0V) de la pin 3
+  delay(0.5); //Délai ajouté pour imposer une fréquence de 1kHz à la boucle
+  #endif
+
+  /* 
+   *  Idée de solution pour tendre vers une valeur réelle de 1kz:
+   *  delay(0.5) -> delay(0.4xx) et ajouter les autres fonction 
+   *  entre afin d'obtenir presque 1 kz
+   */
   //6)
   }
